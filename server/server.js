ce49751d5d9be3823e7ae64012fcd9b5f3c6dee3
cadullira <cadullira@gmail.com>
2025-08-20 14:31:43 -0300

    chore: configura pacote do servidor com dependências e scripts iniciais

--- server/server.js
+++ server/server.js
@@ -1,5 +1,7 @@
 const express = require('express');
 const cors = require('cors');
+const mongoose = require('mongoose'); // Importa o mongoose
+require('dotenv').config(); // Carrega as variáveis de ambiente
 
 const app = express();
 
@@ -7,6 +9,14 @@ app.use(cors());
 app.use(express.json());
 
 const PORT = process.env.PORT || 5000;
+
+// Conexão com o MongoDB
+const uri = process.env.MONGO_URI;
+mongoose.connect(uri, { useNewUrlParser: true, useUnifiedTopology: true });
+const connection = mongoose.connection;
+connection.once('open', () => {
+  console.log("Conexão com o MongoDB estabelecida com sucesso");
+});
 
 app.get('/', (req, res) => {
   res.send('API do Taskfy está funcionado!');
